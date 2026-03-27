2a726cdd8bdfce70b298676146b012fa74733b29
Bo Wang <christinjack_@outlook.com>
2026-03-27 12:04:11 +0800

    feat: add static results dashboard

--- src/tacc/reporting/__init__.py
+++ src/tacc/reporting/__init__.py
@@ -1,3 +1,4 @@
+from tacc.reporting.dashboard import build_dashboard_html
 from tacc.reporting.markdown import build_markdown_report, load_latest_benchmark_result
 
-__all__ = ["build_markdown_report", "load_latest_benchmark_result"]
+__all__ = ["build_dashboard_html", "build_markdown_report", "load_latest_benchmark_result"]
