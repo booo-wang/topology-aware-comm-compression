9ee6ac35a1e048288f04360aef1242e860d1efe5
Bo Wang <christinjack_@outlook.com>
2026-03-30 18:32:49 +0800

    feat: add current findings layer to reporting

--- src/tacc/reporting/__init__.py
+++ src/tacc/reporting/__init__.py
@@ -1,4 +1,5 @@
 from tacc.reporting.dashboard import build_dashboard_html
+from tacc.reporting.findings import build_findings
 from tacc.reporting.markdown import build_markdown_report, load_latest_benchmark_result
 
-__all__ = ["build_dashboard_html", "build_markdown_report", "load_latest_benchmark_result"]
+__all__ = ["build_dashboard_html", "build_findings", "build_markdown_report", "load_latest_benchmark_result"]
